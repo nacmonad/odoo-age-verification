fa6fd136fecf22dcd776fb98842187350bd2a6e7
nacmonad <macnomad84@gmail.com>
2024-03-20 18:41:45 -0400

    products_item test custom template

--- __manifest__.py
+++ __manifest__.py
@@ -4,13 +4,18 @@
     'version': '1.0',
     'category': 'Website',
     'summary': 'Age Verification for Website',
-    'depends': ['website','web'],
+    'depends': ['website','web', 'website_sale'],
     'data': [
         'views/age_verification.xml',
+        'views/template.xml',
+
     ],
     'assets': {
         'web.assets_frontend': [
             'odoo_age_verification/static/src/js/age_verification.js',
         ],
     },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
