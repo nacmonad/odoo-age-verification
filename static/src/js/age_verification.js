863114ec193ad8dc6a35b414e972728cb540ee62
nacmonad <macnomad84@gmail.com>
2024-03-20 15:34:06 -0400

    age_verification.js

--- static/src/js/age_verification.js
+++ static/src/js/age_verification.js
@@ -1,18 +1,18 @@
 odoo.define('age_verification.popup', function(require) {
     'use strict';
-
+    
     var publicWidget = require('web.public.widget');
-
-    publicWidget.Widget.extend({
+    
+    publicWidget.registry.AgeVerificationPopup = publicWidget.Widget.extend({
         selector: ':not([data-age-verified])',
         start: function() {
             if (!localStorage.getItem('isAgeVerified')) {
-                // Here you would actually implement your popup logic
-                // For demonstration, let's just use an alert
                 alert('Hello World! Verify your age.');
                 localStorage.setItem('isAgeVerified', 'true');
-                // You might want to redirect or perform another action after setting the flag
             }
-        }
+            return this._super.apply(this, arguments);
+        },
     });
-});
+    
+    });
+    
