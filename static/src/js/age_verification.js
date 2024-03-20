dc025dcb92e2fb143a6153b865c75da949f301db
nacmonad <macnomad84@gmail.com>
2024-03-20 17:00:28 -0400

    simple jquery way ><

--- static/src/js/age_verification.js
+++ static/src/js/age_verification.js
@@ -1,20 +1,28 @@
 odoo.define('age_verification.popup', [], function(require) {
     'use strict';
-
     const isAgeVerified = localStorage.getItem('isAgeVerified');
-    console.log("[age_verification]isAgeVerified", isAgeVerified)
-
+    console.log("[age_verification]isAgeVerified", isAgeVerified);
 
     if (!isAgeVerified) {
-        alert('By clicking OK, you are verifying you are over the of age 19.');
-        localStorage.setItem('isAgeVerified', 'true');
-    }
+        $('body').prepend(`
+            <div id="age-verification-popup" style="position:fixed;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.5);z-index:9999;display:flex;justify-content:center;align-items:center;">
+                <div style="background:#fff;padding:20px;max-width:400px;text-align:center;">
+                    <p>You must be over the age of 19 to enter this site.</p>
+                    <button id="age-verification-confirm">I am over 19</button>
+                </div>
+            </div>
+        `);
 
-    var publicWidget = require('web.public.widget');
-    var widgetRegistry = require('web.widget_registry');
-    console.log({ publicWidget, widgetRegistry });
+        $('#age-verification-confirm').click(function() {
+            localStorage.setItem('isAgeVerified', 'true');
+            $('#age-verification-popup').hide();
+        });
+        }
+    });
 
-
+    // var publicWidget = require('web.public.widget');
+    // var widgetRegistry = require('web.widget_registry');
+    // console.log({ publicWidget, widgetRegistry });
     // publicWidget.registry.AgeVerificationPopup = publicWidget.Widget.extend({
     //     selector: ':not([data-age-verified])',
     //     start: function() {
@@ -25,4 +33,4 @@ odoo.define('age_verification.popup', [], function(require) {
     //         return this._super.apply(this, arguments);
     //     },
     // });
-});
+//});
